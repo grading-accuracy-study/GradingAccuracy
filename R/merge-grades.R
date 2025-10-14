3d59699e5f09de9891ca7dfaaf0073bf1db97fb1
nikita-jaya <nikita.jp@gmail.com>
2025-10-13 21:31:20 -0400

    deidentify gradescope function

---
+++ R/merge-grades.R
@@ -0,0 +1,34 @@
+#' Deidentify Gradescope
+#' 
+#' This function de-identifies the exported
+#' Gradescope csv by swapping all columns with
+#' identifiable data with the de-identified IDs
+#' in an existing table. Note that Gradescope csv
+#' must have a column called `Assignment Submission ID` and the ids table
+#' must have two columns: `SID`, `Generated_ID`
+#' 
+#' @param gs_csv_path path to exported Gradescope csv
+#' @param ids_csv_path path to de-identified table csv
+#' @param output_path path to de-idenfitied Gradescope output
+#' 
+#' @importFrom readr read_csv write_csv
+#' @importFrom dplyr left_join mutate select join_by
+deidentify_gradescope <- function(gs_csv_path, ids_csv_path,
+                                  output_path){
+  #ignore last 3 lines
+  nrow = length(readLines(gs_csv_path))-5
+  
+  gs_csv <- read_csv(gs_csv_path, show_col_types = FALSE,
+                     n_max = nrow)
+  ids_csv <- read_csv(ids_csv_path, show_col_types = FALSE)
+  
+  de_identified <- gs_csv |>
+    mutate(`Assignment Submission ID` = as.numeric(`Assignment Submission ID`)) |>
+    left_join(ids_csv, by = join_by("Assignment Submission ID" == "SID")) |>
+    mutate(SID = Generated_ID)
+  
+  de_identified |>
+    select(SID, Score:Comments, Tags) |>
+    write_csv(file = output_path)
+    
+}
