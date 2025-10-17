9067c0d9a4c0ed9cdba784d87679249bba2d3f8a
nikita-jaya <nikita.jp@gmail.com>
2025-10-17 12:43:20 -0400

    read_evals function

--- R/merge-grades.R
+++ R/merge-grades.R
@@ -17,21 +17,49 @@
 #' @export
 deidentify_gradescope_evals <- function(gs_csv_path, ids_csv_path,
                                   output_path){
-  #ignore last 5 lines
-  nrow = length(readLines(gs_csv_path))-5
 
-  gs_csv <- read_csv(gs_csv_path, show_col_types = FALSE,
-                     n_max = nrow)
+  gs_csv <- read_evals(gs_csv_path)
   ids_csv <- read_csv(ids_csv_path, show_col_types = FALSE)
 
   de_identified <- gs_csv |>
     mutate(`Assignment Submission ID` = as.numeric(`Assignment Submission ID`)) |>
     left_join(ids_csv, by = join_by("Assignment Submission ID" == "SID")) |>
-    mutate(SID = Generated_ID)
+    mutate(SID = Generated_ID) |>
+    select(SID, Score:Comments, Tags)
 
-  de_identified |>
-    select(SID, Score:Comments, Tags) |>
-    write_csv(file = output_path)
+    write_evals(de_identified = de_identified, output_path = output_path,
+                original_path = gs_csv_path)
 
 }
 
+
+#' Read Gradescope Evaluations CSV
+#'
+#' This function reads in the .csv file from Gradescope's
+#' "Export Evaluations" functionality. The formatting of this
+#' file is not in tidy format because the last few lines include
+#' Point Values, Rubric Numbers and Scoring Method. These are
+#' excluded for the sake of this package.
+#'
+#' @param csv_path path to exported Gradescope evaluations csv
+#' @param ignore_nrows how many of the last lines to ignore
+#'
+#' @importFrom readr read_csv
+#'
+#' @export
+read_evals <- function(csv_path, ignore_nrows = 5){
+  nrow = length(readLines(gs_csv_path)) - ignore_nrows
+  read_csv(gs_csv_path, show_col_types = FALSE,
+           n_max = nrow)
+}
+
+#' @importFrom readr write_csv
+write_evals <- function(de_identified, output_path,
+                        ignored_nrows = 5, original_path){
+  # append remove lines to maintain original format
+  write_csv(de_identified, output_path)
+  deidentified_lines <- readLines(output_path)
+  last_lines <- tail(readLines(original_path), ignored_nrows-1)
+  writeLines(c(deidentified_lines, last_lines), output_path)
+
+}
