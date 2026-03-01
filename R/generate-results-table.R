0ec4c0def4b8f8fa8d69936ddd6c676370034a71
nikita-jaya <nikita.jp@gmail.com>
2026-03-01 13:19:50 -0500

    diff cols

--- R/generate-results-table.R
+++ R/generate-results-table.R
@@ -157,12 +157,13 @@ generate_metadata_row <- function(dir){
   course_info <- metadata$course_info
   tibble::tibble(
     `Question Name` = basename(dir),
-    `Subject` = course_info$department,
+    # `Subject` = course_info$department,
     `Course` = paste(course_info$department, course_info$course_number),
     `Course Name` = course_info$course_name,
     `Course Level` = ifelse(course_info$upper_div,
                             "Upper Division", "Lower Division"),
     `Semester` = paste(course_info$semester, course_info$year),
+    `Question Number` = paste(course_info$assignment_name, course_info$question_number),
     `Mode of Question` = course_info$mode_of_question,
     `Medium of Answer` = course_info$medium_of_answer,
     `Content of Answer` = course_info$content_of_answer
