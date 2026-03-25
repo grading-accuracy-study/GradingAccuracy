ff7d8df56304c730a60b35165551ca13f551ff00
nikita-jaya <nikita.jp@gmail.com>
2026-03-25 08:57:13 -0400

    typo

--- R/computing-accuracy.R
+++ R/computing-accuracy.R
@@ -188,7 +188,7 @@ find_differences_wrt_students <- function(experts_file, student_file,
   student_eval <- readr::read_csv(student_file, show_col_types = F)
 
   # find expert vs. AI differences to remove
-  ai_errors <- rownames(ai_diffs)
+  ai_errors <- rownames(ai_diffs$mismatch_matrix)
   experts_eval <- experts_eval |>
     filter(!(SID %in% ai_errors))
   student_eval <- student_eval |>
